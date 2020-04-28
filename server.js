5d44e4b6299bb52ca82d6b6183fc9b7e800ec38b
famesensor <famfew25@gmail.com>
2020-04-29 03:19:53 +0700

    [update]: add commit, add route auth user

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
-require("dotenv").config();
-const express = require("express");
-const bodyParser = require("body-parser");
-const passport = require("passport");
-const cors = require("cors");
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const cors = require('cors');
+const errorHandler = require('./middleware/error');
 
 const app = express();
-
+//
+// ─── MIDDLEWARE ─────────────────────────────────────────────────────────────────
+//
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-app.use(passport.initialize());
 app.use(cors());
+
+//
+// ─── ROUTE ──────────────────────────────────────────────────────────────────────
+//
+const auth = require('./routes/api/auth');
+app.use('/api/auth/', auth);
+//
+// ─── ERRORHANDLER ───────────────────────────────────────────────────────────────
+//
+app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
 
